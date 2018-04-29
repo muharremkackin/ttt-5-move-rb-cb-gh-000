c3b3ac214657d13447e8b2527c7f21298ebd2015
Muharrem Kaçkın <muharremkackin@gmail.com>
2018-04-29 11:45:57 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,6 +8,11 @@ end
 
 # code your input_to_index and move method here!
 
-def update_array_at_with(array, index, value)
+def input_to_index(array, index, value)
   array[index] = value
 end
+
+def move
+
+end
+
