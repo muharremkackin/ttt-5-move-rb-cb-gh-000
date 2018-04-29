5e577387753b8c78b6fa8d785b218ae7337c223f
Muharrem Kaçkın <muharremkackin@gmail.com>
2018-04-29 11:47:36 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,8 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(array, index, value)
-  array[index] = value
+def input_to_index(string)
+  return string*1
 end
 
 def move
